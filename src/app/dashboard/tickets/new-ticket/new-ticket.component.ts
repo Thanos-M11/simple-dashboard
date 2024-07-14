1ec517e51377d85e88b4f71814d26e7026c6dcfe
Thanos-M11 <kalaitzisa@gmail.com>
2024-07-14 13:54:21 +0300

    update new ticket, reset form woth viewChild method

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -15,8 +15,11 @@ export class NewTicketComponent {
   // onSubmit(titleElement: HTMLInputElement) {
   //   console.dir(titleElement);
   // }
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
   onSubmit(title: string, text: string) {
     console.log(title);
     console.log(text);
+    this.form().nativeElement.reset();
   }
 }
