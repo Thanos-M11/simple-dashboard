115141a971406470e5643929b8a499a50ba80560
Thanos-M11 <kalaitzisa@gmail.com>
2024-07-14 13:29:39 +0300

    update new ticket , added template variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
+import { AnimationTriggerNames } from '@angular/compiler';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  // onSubmit(titleElement: HTMLInputElement) {
+  //   console.dir(titleElement);
+  // }
+  onSubmit(title: string, text: string) {
+    console.log(title);
+    console.log(text);
+  }
+}
